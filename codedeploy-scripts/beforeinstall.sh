59ca7be40e2053c66206a61b12ce8dd5e50aaa6d
cmilsor245 <cmilsor245@g.educaand.es>
2024-02-07 14:48:53 +0100

    change file name

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -5,3 +5,5 @@ chmod +x /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 ./gradlew build
 ./gradlew war
+
+mv build/libs/holamundo-0.0.1-SNAPSHOT.war /build/libs/holamundo2.war
